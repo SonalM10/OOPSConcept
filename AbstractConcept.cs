4e6a4531a0192c7644926d51f0463e7d37bca0eb
Sonal More <sonal.more10@hotmail.com>
2022-09-14 01:46:28 +0530

    Add project files.

---
+++ AbstractConcept.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSConcept
+{
+    class AbstractConcept
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what is diff between abtract class and interface");
+            PermentEmployee permentEmployee = new PermentEmployee();
+             permentEmployee.Project();
+             permentEmployee.Project1();
+             permentEmployee.Role();
+             permentEmployee.Role1();
+              Console.ReadLine();
+        }
+
+    }
+    public abstract class Employees
+    {
+      
+         public Employees()
+        {
+
+        }
+        public string Name;
+        public abstract void Project(); //Method Declared
+        public void Role() //Method Defined
+        {
+            Console.WriteLine("Software Engineer");
+            //body present in this method 
+        }
+    }
+    interface IEmployee
+    {
+        //only Declaration allow
+        public void Project();
+        public void Role();
+    }
+    interface IEmployee1
+    {
+        //only Declaration allow
+        public void Project1();
+        public void Role1();
+    }
+    public class PermentEmployee : Employees, IEmployee,IEmployee1
+    {
+        public override void Project()
+        {
+            Console.WriteLine("Project CITY Bank");
+        }
+        public void Role()
+        {
+            Console.WriteLine("Manager");
+        }
+        public  void Project1()
+        {
+            Console.WriteLine("Project Sun");
+        }
+        public void Role1()
+        {
+            Console.WriteLine("Devloper");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace OOPSConcept
+{
+    public class Employee //class --- //base class / parent class / super class
+    {
+        public Employee() //constructor
+        { 
+            //code
+        }
+      
+        private float experince;// field 
+        public float Experince //properties
+        {
+            get { return experince; }
+            set { experince = value; }
+        }
+        public void CalculateSalary() //method
+        {
+            int salary = (int)Experince * 300000;
+            Console.WriteLine("Salary: {0}",salary);
+        }
+        public class PermentEmployee : Employee //Derive/child/ sub class
+        {
+
+        }
+        public class ContractEmployee : Employee //Derive/child/ sub class
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            PermentEmployee employee = new PermentEmployee();
+            Console.WriteLine("Please Enter the Experince:");
+            employee.Experince = (float)Convert.ToDecimal(Console.ReadLine());
+            //employee.Experince = 10;
+            employee.CalculateSalary();
+            Console.ReadKey();
+        }
+
+       
+    }
+   
+}
